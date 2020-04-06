911cf9cc172fafcb16e19f888650f12976d01ddd
grountish <grountish@gmail.com>
2020-04-06 17:18:20 +0200

    linked to FB, fixed minor bugs

--- scripts/stats.js
+++ scripts/stats.js
@@ -4,13 +4,37 @@ let usersDB = JSON.parse(localStorage.getItem('score'))
 let tbody = document.querySelector('tbody')
 
 
-usersDB.forEach(puntuation => { 
-   let newTr = document.createElement('tr')
-   newTr.innerHTML = `<td id="firstPlace" class="info-text ">${puntuation.name}</td>
-   <td id="firstScore" class="info-text pr-5 text-right">${puntuation.score}</td>`
-    tbody.appendChild(newTr)
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyB5-avS8qpLLRa2KIhyRSwvIvhYNIh1-s4",
+    authDomain: "testing-proyect-a9124.firebaseapp.com",
+    databaseURL: "https://testing-proyect-a9124.firebaseio.com",
+    projectId: "testing-proyect-a9124",
+    storageBucket: "testing-proyect-a9124.appspot.com",
+    messagingSenderId: "858551138090",
+    appId: "1:858551138090:web:70b27c90b9781ee0e11053"
+};
+
+firebase.initializeApp(firebaseConfig)
+var database = firebase.database();
+ref = database.ref('testing-proyect-a9124')
+ref.on('value', gotData, errData)
+
+// Show Score comming from DB
+function gotData(data) {
+    let scores = data.val()
+    let valuex = Object.values(scores)
+    let sorted = valuex.sort((a,b)=> b.score - a.score)
+    sorted.forEach((puntuation) => {
+        
+        let newTr = document.createElement('tr')
+        newTr.innerHTML = `<td id="firstPlace" class="info-text ">${puntuation.name}</td>
+        <td id="firstScore" class="info-text pr-5 text-right">${puntuation.score}</td>`
+        tbody.appendChild(newTr)
     
-});
+    });
+}
 
-
-//firstPlaceScore.innerText = usersDB2[usersDB2.length-1].score 
+function errData(err) {
+    console.log(err);
+}
